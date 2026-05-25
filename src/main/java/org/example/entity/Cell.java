97f10a24d3018d7160a265eb81d275c701408a78
dimal <dimalitvin192.0@gmail.com>
2026-05-25 20:13:52 +0200

    returned the previous implementation of the isClosed method

--- src/main/java/org/example/entity/Cell.java
+++ src/main/java/org/example/entity/Cell.java
@@ -1,10 +1,13 @@
 package org.example.entity;
 
 import lombok.*;
+import org.example.utils.EdgeUtils;
+import org.example.utils.VectorUtils;
 
 import java.util.Objects;
 
 import static org.example.utils.EdgeUtils.getVertexOfTangency;
+import static org.example.utils.EdgeUtils.isEquals;
 
 @Getter
 @Setter
@@ -15,4 +18,17 @@ public class Cell {
     @EqualsAndHashCode.Include
     private Vertex center;
     private Edge boundary;
+
+    public boolean isClosed() {
+        Edge nextEdge = boundary;
+        do {
+            nextEdge = nextEdge.getNext();
+            if (nextEdge == null) {
+                return false;
+            }
+            if(EdgeUtils.isEquals(boundary, nextEdge)) {
+                return true;
+            }
+        } while (true);
+    }
 }
